b96a937f7844dc9901911c58194ed640b59121cc
Magzhan Yeleup <magzhan9292@gmail.com>
2022-10-10 11:24:27 +0600

    First init

---
+++ download.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+rm -rf app
+
+while IFS= read -r line; do
+
+    name=$(echo $line| cut -d ' ' -f1)
+    path=$(echo $line| cut -d ' ' -f2)
+
+    if [ "$name" = "$1" ]; then
+      rm -rf download/"$path"
+      mkdir -p download/"$path"
+      (
+        cd download
+        mkdir -p $path
+        unzip "$path".zip -d $path
+      )
+      mkdir -p app/public
+      cp -r download/"$path"/upload/* app/public
+      rm -rf download/"$path"
+
+      touch app/public/config.php app/public/admin/config.php
+      rm app/public/config-dist.php app/public/admin/config-dist.php
+
+      chmod -R 777 app
+    fi
+done < ./download.txt
